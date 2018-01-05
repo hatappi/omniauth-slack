55c721208483d88956a29fdd6e4a654a9cef5f2a
Yusaku Hatanaka <hata.yusaku.1225@gmail.com>
2018-01-05 23:05:17 +0900

    add info

--- lib/omniauth/strategies/slack.rb
+++ lib/omniauth/strategies/slack.rb
@@ -22,22 +22,78 @@ module OmniAuth
       # User ID is not guaranteed to be globally unique across all Slack users.
       # The combination of user ID and team ID, on the other hand, is guaranteed
       # to be globally unique.
-      uid { "#{user_identity['id']}-#{team_identity['id']}" }
+      uid { "#{auth['user_id'] || auth['user'].to_h['id']}-#{auth['team_id'] || auth['team'].to_h['id']}" }
 
       info do
-        hash = {
-          name: user_identity['name'],
-          email: user_identity['email'],    # Requires the identity.email scope
-          image: user_identity['image_48'], # Requires the identity.avatar scope
-          team_name: team_identity['name']  # Requires the identity.team scope
+        # Start with only what we can glean from the authorization response.
+        hash = { 
+          name: auth['user'].to_h['name'],
+          email: auth['user'].to_h['email'],
+          user_id: auth['user_id'] || auth['user'].to_h['id'],
+          team: auth['team_name'] || auth['team'].to_h['name'],
+          team_id: auth['team_id'] || auth['team'].to_h['id'],
+          image: auth['team'].to_h['image_48']
         }
 
+        # Now add everything else, requiring further calls to the api, if necessary.
         unless skip_info?
-          [:first_name, :last_name, :phone].each do |key|
-            hash[key] = user_info['user'].to_h['profile'].to_h[key.to_s]
+          %w(first_name last_name phone skype avatar_hash real_name real_name_normalized).each do |key|
+            hash[key.to_sym] = (
+              user_info['user'].to_h['profile'] ||
+              user_profile['profile']
+            ).to_h[key]
           end
-        end
 
+          %w(deleted status color tz tz_label tz_offset is_admin is_owner is_primary_owner is_restricted is_ultra_restricted is_bot has_2fa).each do |key|
+            hash[key.to_sym] = user_info['user'].to_h[key]
+          end
+
+          more_info = {
+            image: (
+              hash[:image] ||
+              user_identity.to_h['image_48'] ||
+              user_info['user'].to_h['profile'].to_h['image_48'] ||
+              user_profile['profile'].to_h['image_48']
+              ),
+            name:(
+              hash[:name] ||
+              user_identity['name'] ||
+              user_info['user'].to_h['real_name'] ||
+              user_profile['profile'].to_h['real_name']
+              ),
+            email:(
+              hash[:email] ||
+              user_identity.to_h['email'] ||
+              user_info['user'].to_h['profile'].to_h['email'] ||
+              user_profile['profile'].to_h['email']
+              ),
+            team:(
+              hash[:team] ||
+              team_identity.to_h['name'] ||
+              team_info['team'].to_h['name']
+              ),
+            team_domain:(
+              auth['team'].to_h['domain'] ||
+              team_identity.to_h['domain'] ||
+              team_info['team'].to_h['domain']
+              ),
+            team_image:(
+              auth['team'].to_h['image_44'] ||
+              team_identity.to_h['image_44'] ||
+              team_info['team'].to_h['icon'].to_h['image_44']
+              ),
+            team_email_domain:(
+              team_info['team'].to_h['email_domain']
+              ),
+            nickname:(
+              user_info.to_h['user'].to_h['name'] ||
+              auth['user'].to_h['name'] ||
+              user_identity.to_h['name']
+              ),
+          }
+          
+          hash.merge!(more_info)
+        end
         hash
       end
 
